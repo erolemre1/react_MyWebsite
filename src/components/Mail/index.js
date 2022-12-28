bb95a510a6dd979031709e7d6a060747aa5adc6e
emregulec <emre.gulec@emlakjet.com>
2022-12-29 00:27:45 +0300

    Edit styles

--- src/components/Mail/index.js
+++ src/components/Mail/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 import ReactWhatsapp from 'react-whatsapp';
+import { useState } from 'react';
 
 import './styles.scss';
 
 export const ContactUs = () => {
+    const [messages, setMessages] = useState(false)
 
     function sendEmail(e) {
         e.preventDefault();
         emailjs.sendForm('service_ts8mivtasda', 'template_obs88mk', e.target, 'm6MXi-5C8-mX0ZrnJ')
             .then((result) => {
-                console.log(result.text);
+                setMessages(true)
             }, (error) => {
                 console.log(error.text);
             });
         e.target.reset()
+
+        setTimeout(() => {
+            setMessages(false)
+        }, 4000);
+
     }
     return (
         <div>
@@ -38,13 +45,14 @@ export const ContactUs = () => {
                             <textarea className="form-control" id="" cols="30" rows="8" style={{ height: 100 }} placeholder="Your message" name="message"></textarea>
                         </div>
                         <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Mesajı Gönder"></input>
+                            {!messages && <div className="pb-3 mx-auto text-success h4">Mesajınız Gönderildi!</div>}
+                            <input type="submit" className="btn mb-5 btn-info" value="Mesajı Gönder"></input>
                         </div>
                     </div>
                 </form>
             </div>
-            <div className='whatsappWrapper'>
-                <ReactWhatsapp number="5318105767" message="Merhaba" className='whatsapp' >
+            <div className='whatsappWrapper pb-5 d-flex justify-content-lg-end justify-content-center'>
+                <ReactWhatsapp number="5318105767" message="Merhaba" className='whatsapp d-flex flex-column mr-5' >
                     <i class="fab fa-whatsapp"></i>
                     <span>Bana whatsaptan ulaşabilirsiniz</span>
                 </ReactWhatsapp></div>
