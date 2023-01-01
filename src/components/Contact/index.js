6ab1c9e42894a5798a6c3d46c24c3997d5270418
emregulec <emre.gulec@emlakjet.com>
2023-01-01 23:14:40 +0300

    Edit meta

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,22 +5,16 @@ import './style.scss'
 
 const Contact = () => {
     return (
-        <div>   <ul>
-                        <li>
-                            <a href="https://www.instagram.com/emregulec._/?hl=tr" target="_blank"><i class="fab fa-instagram"></i></a>
-                        </li>
-
-                        <li>
-                            <a href="https://twitter.com/ErolEmreGle1 " target="_blank"> <i class="fab fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://tr.linkedin.com/in/erol-emre-gulec?challengeId=AQFHIMoect6XnAAAAX29QiAYrLRRuqhHPRk82ox5cr2Af2koevXoJuxUSxiNxFKWBvS3CIUhKJFR4M8TgZYMM6qh1AkP-MvBNw&submissionId=28ed632f-1ae1-c016-a2b3-7965e1b81260" target="_blank"><i class="fab fa-linkedin"></i> </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/erolemre1" target="_blank"> <i class="fab fa-github"></i></a>
-                        </li>
-                </ul>
-                <ContactUs />
+        <div>
+            <ul>
+                <li>
+                    <a href="https://tr.linkedin.com/in/erol-emre-gulec?challengeId=AQFHIMoect6XnAAAAX29QiAYrLRRuqhHPRk82ox5cr2Af2koevXoJuxUSxiNxFKWBvS3CIUhKJFR4M8TgZYMM6qh1AkP-MvBNw&submissionId=28ed632f-1ae1-c016-a2b3-7965e1b81260" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i> </a>
+                </li>
+                <li>
+                    <a href="https://github.com/erolemre1" target="_blank" rel="noreferrer"> <i class="fab fa-github"></i></a>
+                </li>
+            </ul>
+            <ContactUs />
         </div>
     )
 }
