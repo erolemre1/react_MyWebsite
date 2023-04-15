078ace658ba66ad13b03554ba569b52327a942d1
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-15 23:03:05 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,39 +3,25 @@ import './hstyle.scss'
 
 const About = () => {
   
-  const handleDeeplinkClick = (url, appStoreUrl) => {
-    // Attempt to open the deeplink URL
-    const appWindow = window.location.href = url
-  
-    // Check if the app has opened
-    const checkAppOpened = () => {
-      // Check if the app window is focused
-      if (appWindow && appWindow.window && appWindow.window.focus) {
-        // The app has opened and the deeplink worked
-        console.log('App is installed');
-        // Close the app window
-        appWindow.close();
-      } else {
-        // The app has not opened yet, or the deeplink failed
-        console.log('App is not installed');
-        // Redirect to the app store
-        window.location.href = appStoreUrl;
-      }
-    };
-  
-    // Check if the app has opened when the current window loses focus
-    window.addEventListener('blur', checkAppOpened);
-  };
+  function isAppInstalled() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    return userAgent.includes("emlakjet");
+  }
 
+const  handleDeeplinkClick = () => {
+    if (isAppInstalled()) {
+      window.location.href = "emlakjetapp://path/to/content";
+    } else {
+      window.location.href = "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334";
+    }
+  };
+  
   return (
     <div className="container" >
       <button
         onClick={() => {
           setTimeout(() => {
-            handleDeeplinkClick(
-              'emlakjetapp://',
-              'https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334',
-            );
+            handleDeeplinkClick();
           }, 500);
         }}
       >
