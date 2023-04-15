fc7e78c4f6889e15673028a8b342568647e6da66
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-15 23:26:10 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,43 +5,22 @@ import './hstyle.scss'
 const About = () => {
   const [first, setfirst] = useState('asd')
   useEffect(() => {
-    setfirst(window.location.href.match(/emlakjetapp/i))
+    setfirst(navigator.userAgent)
   }, [])
   
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-
-  function isMobileAppInstalled() {
-    if (/android/i.test(userAgent)) {
-      return !!window.location.href.match(/emlakjetapp/i);
-    }
-    if (/iPad|iPhone|iPod/i.test(userAgent)) {
-      return !!window.location.href.match(/emlakjetapp/i);
-    }
-    return false;
-  }
-
+  
   const handleDeeplinkClick = () => {
 
-    if (isMobileAppInstalled()) {
-      // Uygulama açma işlemi
-      window.location.href = 'emlakjetapp://';
-    } else {
-      // Uygulama indirme sayfasına yönlendirme işlemi
-      if (/android/i.test(userAgent)) {
-        window.location.href = 'https://play.google.com/store/apps/details?id=com.emlakjet.kurumsal.sekizbit&hl=tr&gl=US';
-      }
-      if (/iPad|iPhone|iPod/i.test(userAgent)) {
-        window.location.href = 'https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334';
-      }
-    }
+    window.location.href = "emlakjetapp://";
+    setTimeout(function () {
+      window.location.href = 'https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334';
+    }, 25);
   }
 
 
   return (
     <div className="container" >
       {first}
-      asdsds
       <button
         onClick={() => {
           setTimeout(() => {
