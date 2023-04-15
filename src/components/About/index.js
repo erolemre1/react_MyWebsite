e8a1a27b37e59c55cc65dae9707178e112ca4ae8
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-15 23:54:23 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,37 +8,45 @@ const About = () => {
     setfirst(navigator.userAgent)
   }, [])
 
-  function getMobileOperatingSystem() {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    
-    if (/android/i.test(userAgent)) {
-      return "Android";
+  const appUrl = "emlakjetapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
+  const storeUrl = "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334"; // Uygulamanızın App Store veya Google Play Store bağlantısı
+  
+  // Kullanıcının cihazında uygulama yüklü mü diye kontrol edin
+  function isAppInstalled() {
+    let isInstalled = false;
+  
+    // Özel URL şemasını kullanarak kontrol edin
+    window.location.href = appUrl;
+  
+    // Kullanıcı uygulamayı açmaya çalışırsa isInstalled değişkeni true olur
+    setTimeout(function () {
+      if (!isInstalled) {
+        isInstalled = false;
+      }
+    }, 1000);
+  
+    // Kullanıcının cihazına geri yönlendirin
+    window.location.href = "http://www.example.com";
+  
+    return isInstalled;
+  }
+  
+  // Kullanıcının cihazında uygulama yüklüyse uygulamayı açın, değilse uygulama mağazasına yönlendirin
+  const openApp = () => {
+
+    if (isAppInstalled()) {
+      window.location.href = appUrl;
+    } else {
+      window.location.href = storeUrl;
     }
     
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-      return "iOS";
-    }
-    
-    return "unknown";
   }
 
-  let appLink = "emlakjetapp://";
-
-function openApp() {
-  window.location = appLink;
-  
-  setTimeout(function() {
-    if (!document.webkitHidden) {
-      window.location = "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334";
-    }
-  }, 2000);
-}
-
-if (getMobileOperatingSystem() === "iOS") {
-  appLink = "emlakjetapp://";
-} else if (getMobileOperatingSystem() === "Android") {
-  appLink = "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
-}
+// if (getMobileOperatingSystem() === "iOS") {
+//   appLink = "emlakjetapp://";
+// } else if (getMobileOperatingSystem() === "Android") {
+//   appLink = "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
+// }
 
   return (
     <div className="container" >
