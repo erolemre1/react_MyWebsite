af3b73ade3d3661a777224743e3aec776d5edeb2
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-15 23:36:08 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,16 +7,45 @@ const About = () => {
   useEffect(() => {
     setfirst(navigator.userAgent)
   }, [])
-  
-  
-  const handleDeeplinkClick = () => {
-
-    window.location.href = "emlakjetapp://";
-    setTimeout(function () {
-      window.location.href = 'https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334';
-    }, 25);
+  function isAppInstalled() {
+    // iOS için kontrol
+    if (/iP(hone|ad|od)/.test(navigator.platform)) {
+      // iOS için URL şemasını oluşturma
+      const appUrlScheme = "emlakjetapp://";
+      const appStoreLink = "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334";
+      // URL şemasını kullanarak uygulamayı açma denemesi
+      const iframe = document.createElement("iframe");
+      iframe.setAttribute("src", appUrlScheme);
+      iframe.setAttribute("style", "display:none;");
+      document.body.appendChild(iframe);
+      // Uygulama açılmazsa, App Store sayfasına yönlendirme
+      setTimeout(function() {
+        document.body.removeChild(iframe);
+        window.location.href = appStoreLink;
+      }, 2000);
+    }
+    // Android için kontrol
+    else if (/Android/.test(navigator.userAgent)) {
+      // Android için URL şemasını oluşturma
+      const appUrlScheme = "emlakjetapp://";
+      const playStoreLink = "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334";
+      // URL şemasını kullanarak uygulamayı açma denemesi
+      const iframe = document.createElement("iframe");
+      iframe.setAttribute("src", appUrlScheme);
+      iframe.setAttribute("style", "display:none;");
+      document.body.appendChild(iframe);
+      // Uygulama açılmazsa, Play Store sayfasına yönlendirme
+      setTimeout(function() {
+        document.body.removeChild(iframe);
+        window.location.href = playStoreLink;
+      }, 3000);
+    }
+    // Diğer cihazlarda kontrol yapmama gerek yok
+    else {
+      return false;
+    }
   }
-
+  
 
   return (
     <div className="container" >
@@ -24,7 +53,7 @@ const About = () => {
       <button
         onClick={() => {
           setTimeout(() => {
-            handleDeeplinkClick();
+            isAppInstalled();
           }, 500);
         }}
       >
