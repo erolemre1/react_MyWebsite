61d50f2215103aedabe6a635051473501f83d6cf
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-15 21:50:06 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './hstyle.scss'
 
-const handleDeeplinkClick = (url, appStoreUrl) => {
-  // Check if the app is installed
-  const isAppInstalled = window.navigator.userAgent.indexOf('emlakjet') > -1;
-  if (isAppInstalled) {
-    // Open the app with the deeplink
-    window.location.href = url;
-  } else {
-    // Redirect to the app store
-    window.location.href = appStoreUrl;
-  }
-};
 const About = () => {
+  const [first, setfirst] = useState(null);
+  
+
+  const handleDeeplinkClick = (url, appStoreUrl) => {
+    // Check if the app is installed
+    const isAppInstalled = window.navigator.userAgent.indexOf('emlakjet') > -1;
+    console.log("window.navigator11")
+    console.log("window.navigator",window.navigator)
+    if (isAppInstalled) {
+      // Open the app with the deeplink
+      window.location.href = url;
+    } else {
+      // Redirect to the app store
+      window.location.href = appStoreUrl;
+    }
+    setfirst(window.navigator.userAgent)
+  };
+
   return (
     <div className="container" >
+      <div>
+        {first}
+      </div>
       <button
         onClick={() => {
           setTimeout(() => {
