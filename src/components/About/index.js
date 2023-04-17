378b575dc65ee956ac7c35ee9c3f5ae2af92594d
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-17 14:54:31 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,43 +8,18 @@ const About = () => {
     setfirst(navigator.userAgent)
   }, [])
 
-  const IosDeepUrl = "emlakjetapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
+  // const IosDeepUrl = "emlakjetapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
   // const androidDeepUrl = "ejapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
   const IosStoreUrl = "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334"; // Uygulamanızın App Store veya Google Play Store bağlantısı
   // const androidStoreUrl = "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
- 
-  function isAppInstalled(uriScheme) {
-    let isInstalled;
-    if (navigator.userAgent.match(/(iPad|iPhone|iPod touch);.*CPU.*OS 7_\d/i)) {
-      isInstalled = false;
-      const appUrl = uriScheme;
-      const iframe = document.createElement('iframe');
-      iframe.setAttribute('src', appUrl);
-      iframe.setAttribute('style', 'display:none;');
-      document.body.appendChild(iframe);
-      window.setTimeout(function() {
-        document.body.removeChild(iframe);
-        if (!isInstalled) {
-          // Uygulama yüklü değil, store linkine yönlendirme yapabilirsiniz
-          window.location = IosStoreUrl
-        }
-      }, 2000);
-      window.onblur = function() {
-        isInstalled = true;
-      };
-    } else {
-      // iOS 7'den önceki sürümlerde, uygulama kontrolü için canlı test gereklidir.
-      window.location.href = uriScheme;
-    }
-  }
+
   
   // Kullanıcının cihazında uygulama yüklüyse uygulamayı açın, değilse uygulama mağazasına yönlendirin
   const openApp = () => {
-    if (isAppInstalled(IosDeepUrl)) {
-      window.location.href = IosDeepUrl; // Uygulamayı aç
-    } else {
-      window.location.href = IosStoreUrl; // Store sayfasına yönlendir
-    }
+  setTimeout(() => {
+    
+    window.location.href = IosStoreUrl; // Store sayfasına yönlendir
+  }, 700);
 
   }
 
@@ -62,11 +37,11 @@ const About = () => {
         <meta name="apple-itunes-app" content="app-id=1551825141" />
     </head>
       {first}
-      <button
+      <a href="emlakjetapp://"
         onClick={() => openApp()}
       >
         Open mobile app
-      </button>
+      </a>
     </div>
   )
 };
