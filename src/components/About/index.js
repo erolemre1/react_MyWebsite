2ef496840ddd5fb5fe96a7980f9b4f0aad2740a1
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-17 15:48:17 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,39 +8,26 @@ const About = () => {
     setfirst(navigator.userAgent)
   }, [])
 
-  const IosDeepUrl = "emlakjetapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
-  const androidDeepUrl = "ejapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
+  // const IosDeepUrl = "emlakjetapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
+  // const androidDeepUrl = "ejapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
   const IosStoreUrl = "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334?l=tr" // Uygulamanızın App Store veya Google Play Store bağlantısı
-  const androidStoreUrl = "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
+  // const androidStoreUrl = "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
 
   
   // Kullanıcının cihazında uygulama yüklüyse uygulamayı açın, değilse uygulama mağazasına yönlendirin
   const openApp = () => {
-    function isAppInstalled() {
-      const userAgent = navigator.userAgent.toLowerCase();
-      const isIOS = /iphone|ipad|ipod/.test(userAgent);
-      const isAndroid = /android/.test(userAgent);
-    
-      if (isIOS) {
-        if(window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.Emlakjet){
-window.location = IosDeepUrl
-        } else {
-window.location = IosStoreUrl
-          
-        }
-      } else if (isAndroid) {
-        if(window.Android && window.Android.Emlakjet){
-          window.location = androidDeepUrl
-                  } else {
-          window.location = androidStoreUrl
-                    
-                  }
-      } else {
-        return false;
-      }
-    }
+    const myLink = document.getElementById('myLink');
 
-    isAppInstalled();
+    myLink.addEventListener('click', function(event) {
+      event.preventDefault(); // Sayfanın yenilenmesini önlemek için tıklamayı iptal et
+    
+      window.location.href = myLink.href; // Deep link URL'sine yönlendirin
+    });
+
+
+    setTimeout(() => {
+      window.location.href = IosStoreUrl
+    }, 500);
   }
 
 // if (getMobileOperatingSystem() === "iOS") {
@@ -57,11 +44,11 @@ window.location = IosStoreUrl
         <meta name="apple-itunes-app" content="app-id=1551825141" />
     </head>
       {first}
-      <button
+      <a id="myLink" href="emlakjetapp://"
         onClick={() => openApp()}
       >
         Open mobile app
-      </button>
+      </a>
     </div>
   )
 };
