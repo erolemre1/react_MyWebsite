565e8f073155bb8f864a96d0f9cfb0cba15d6d5c
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-17 18:09:23 +0300

    new commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,17 +16,14 @@ const About = () => {
 
   // Kullanıcının cihazında uygulama yüklüyse uygulamayı açın, değilse uygulama mağazasına yönlendirin
   const openApp = () => {
-    console.log("navigator.userAgent",navigator)
     window.location = IosDeepUrl
 
    setTimeout(() => {
     // eslint-disable-next-line eqeqeq
-    if (navigator.userAgent.indexOf("Safari") != -1) {
+    if (!navigator.userAgent.toLowerCase().indexOf("chrome") > -1) {
       if (confirm("App Store ile aç")) {
         window.location.href = IosStoreUrl;
-        // Kullanıcı "Tamam" düğmesine tıkladı
       } else {
-        // Kullanıcı "İptal" düğmesine tıkladı
         window.location.reload();
       }
   } else {
