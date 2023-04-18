8c2f16ce1f098b89dfc05bfb7b34370400591f21
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-18 13:44:43 +0300

    new

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,45 +8,41 @@ const About = () => {
   useEffect(() => {
     setfirst(navigator.userAgent);
   }, []);
+  // emlakjet apps deeplinks
+  const IosDeepLink = 'emlakjetapp://';
+  const androidDeepLink = 'ejapp://';
+  const IosStoreUrl = 'https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334?l=tr';
+  const androidStoreUrl = 'intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end';
 
-  const IosDeepUrl = "emlakjetapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
-
-  // const androidDeepUrl = "ejapp://"; // Uygulamanız için belirlediğiniz özel URL şeması
-  const IosStoreUrl =
-    "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334?l=tr"; // Uygulamanızın App Store veya Google Play Store bağlantısı
-  // const androidStoreUrl = "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
-
-  // Kullanıcının cihazında uygulama yüklüyse uygulamayı açın, değilse uygulama mağazasına yönlendirin
-  // const openApp = () => {
-  //   console.log("navigator.userAgent", navigator);
-  //   window.location = IosDeepUrl;
-  //   setTimeout(() => {
-  //     if (confirm("App Store ile aç")) {
-  //       window.location.href = IosStoreUrl;
-  //       // Kullanıcı "Tamam" düğmesine tıkladı
-  //     } else {
-  //       // Kullanıcı "İptal" düğmesine tıkladı
-  //       window.location.reload();
-  //     }
-  //   }, 10);
-  // };
-  const isChrome = navigator.userAgent.toLowerCase().indexOf("crios") > -1;
-
-  const handleOpenApp = () => {
-    if (isChrome) {
-      window.location = IosDeepUrl;
+  const setLink = (deeplink, storeUrl) => {
+    const isChrome = navigator.userAgent.toLowerCase().indexOf('crios') > -1;
+    if (deeplink === androidDeepLink || (deeplink === IosDeepLink && isChrome)) {
+      window.location = deeplink;
     }
     setTimeout(() => {
-      window.location = IosStoreUrl;
+      window.location.href = storeUrl;
     }, 500);
   };
 
+  const openApp = () => {
+    if (/android/i.test(navigator.userAgent)) {
+      setLink(androidDeepLink, androidStoreUrl);
+    } else if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+      setLink(IosDeepLink, IosStoreUrl);
+    }
+  };
+
+  // const closeBanner = () => {
+  //   const now = new Date().getTime();
+  //   localStorage.setItem('smartBannerTime', JSON.stringify(now));
+  // };
+
+
   return (
     <div className="container">
-      <p>{isChrome ? "criosss" : "safffffaarii"}</p>
 
       {first}
-      <button onClick={() => handleOpenApp(IosDeepUrl, IosStoreUrl)}>
+      <button onClick={() => openApp()}>
         Open mobile app
       </button>
     </div>
