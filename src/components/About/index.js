864b9361f56abb6f864212d69fc2512d1e2c10cb
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-25 19:56:51 +0300

    trial open app

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,17 @@
-import './hstyle.scss'
+import { useEffect } from "react";
+import { useState } from "react";
+import "./hstyle.scss";
 // import Imgg from "./me.jpeg"
 
-
-const IosDeepLink = 'emlakjetapp://';
-const androidDeepLink = 'ejapp://';
-const IosStoreUrl = 'https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334?l=tr';
-const androidStoreUrl = 'intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end';
+const IosDeepLink = "emlakjetapp://";
+const androidDeepLink = "ejapp://";
+const IosStoreUrl =
+  "https://apps.apple.com/tr/app/emlakjet-emlak-ara-i-lan-ver/id1194656334?l=tr";
+const androidStoreUrl =
+  "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
 
 const setLink = (deeplink, storeUrl) => {
-  const isChrome = navigator.userAgent.toLowerCase().indexOf('crios') > -1;
+  const isChrome = navigator.userAgent.toLowerCase().indexOf("crios") > -1;
   if (deeplink === androidDeepLink || (deeplink === IosDeepLink && isChrome)) {
     window.location = deeplink;
   }
@@ -26,13 +29,17 @@ const openApp = () => {
 };
 
 const About = () => {
+  const [device, setDevice] = useState(null);
+
+  useEffect(() => {
+    setDevice(navigator.userAgent);
+  }, []);
   return (
-    <div className="container" >
-     <button onClick={openApp}>
-      Uygulama aç
-     </button>
+    <div className="container">
+      {device}
+      <button onClick={openApp}>Uygulama aç</button>
     </div>
-  )
+  );
 };
 
 export default About;
