7a6e6608663900a391320410b296685e7f6c4238
Emre Gulec <emregulec@MacBook-Pro.local>
2023-04-26 10:58:27 +0300

    trial open app

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,13 +13,13 @@ const androidStoreUrl =
   "intent://android_app/#Intent;scheme=android_app;package=com.emlakjet.kurumsal.sekizbit;end";
 
 const setLink = (deeplink, storeUrl) => {
-  const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+  // const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
   
   //ios sorunları okey
-  const isSafari = navigator.userAgent.indexOf("Safari") !== -1 && navigator.userAgent.indexOf("Version") !== -1;
-  if(!isSafari || (!isFirefox && deeplink === androidDeepLink)){
-    window.location = deeplink;
-}
+//   const isSafari = navigator.userAgent.indexOf("Safari") !== -1 && navigator.userAgent.indexOf("Version") !== -1;
+//   if(!isSafari || (!isFirefox && deeplink === androidDeepLink)){
+//     window.location = deeplink;
+// }
 
   setTimeout(() => {
   window.location.href = storeUrl;
